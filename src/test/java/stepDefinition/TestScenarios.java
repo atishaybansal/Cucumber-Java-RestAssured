5d7b2b77177126ee0bb107a68a7a59170c53b56c
mypc <mypc@192.168.0.4>
2021-07-28 12:21:12 +0530

    Updated the files

--- src/test/java/stepDefinition/TestScenarios.java
+++ src/test/java/stepDefinition/TestScenarios.java
@@ -9,94 +9,123 @@ import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.junit.Assert;
 
 
 public class TestScenarios {
     
-    @Given("Current Weather URL is running")
-    public void current_Weather_URL_is_running() {
-    	RestAssured.baseURI = "https://api.openweathermap.org/data/2.5";
-    }
-    
-    @When("User makes a request with API key and City name and Country code verify the response")
-    public void user_makes_a_request_with_API_key_and_City_name_and_Country_code_verify_the_response() {
-    	Response response = given()
+	
+	RequestSpecification request;
+	Response response;
+	int code;
+	String country;
+	int id;
+	int error_code;
+	String message;
+	ArrayList<String> countries;
+	int listsize;
+	Iterator<String> it;
+	
+	@Given("Open Weather API is up and running")
+	public void open_Weather_API_is_up_and_running() {
+		RestAssured.baseURI = "https://api.openweathermap.org/data/2.5";
+	}
+	
+	@When("User passes the city name {string} and country code {string} and app id {string} to the open weather api")
+	public void user_passes_the_city_name_and_country_code_and_app_id_to_the_open_weather_api(String city_name, String country_code, String app_id) {
+
+		response = given()
                 .header("Content-Type","application/json")
-                .param("q", "London,GB")
-            .param("appid", "1cb6ace31e50401f28b864f0b23fdc68")
+                .param("q", city_name,country_code)
+                .param("appid", app_id)
                .when()
                  .get("/weather");
-       	
-       	String country = response.getBody().path("sys.country");
-       	int code = response.getStatusCode();
-       	int id = response.getBody().path("sys.id");
-       	Assert.assertEquals(code, 200);
-    	Assert.assertEquals(country, "GB");
-       	Assert.assertEquals(id, 2019646);
-    }
+		code = response.getStatusCode();
+		country = response.getBody().path("sys.country");
+       	id = response.getBody().path("sys.id");
+	}
 
-    
-    @When("User makes a request with API key and City name verify the response")
-    public void user_makes_a_request_with_API_key_and_City_name_verify_the_response() {
-    	Response response = given()
+	@Then("Verify the response code is {int}")
+	public void verify_the_response_code_is(int status_code) { 
+       
+		Assert.assertEquals(status_code, code);
+	}
+
+	@Then("Verify the country code and country id in response body is also {string} and {int} respectively")
+	public void verify_the_country_code_and_country_id_in_response_body_is_also_and_respectively(String country_code, int country_id) {
+	    
+    	Assert.assertEquals(country_code, country);
+       	Assert.assertEquals(country_id, id);
+	}
+
+	
+	@When("User passes the city name {string} and app id {string} to the open weather api")
+	public void user_passes_the_city_name_and_app_id_to_the_open_weather_api(String city_name, String app_id) {
+		
+		response = given()
                 .header("Content-Type","application/json")
-                .param("q", "London")
-            .param("appid", "1cb6ace31e50401f28b864f0b23fdc68")
+                .param("q", city_name)
+                .param("appid", app_id)
                .when()
                  .get("/find");
-       	
-       	String country1 = response.getBody().path("list[0].sys.country");
-    	String country2 = response.getBody().path("list[1].sys.country");
-    	String country3 = response.getBody().path("list[2].sys.country");
-       	int code = response.getStatusCode();
-       Assert.assertEquals(code, 200);
-       Assert.assertEquals(country1, "GB");
-       Assert.assertEquals(country2, "CA");
-       Assert.assertEquals(country3, "US");
-    }
+		
+		countries = response.getBody().path("list.sys.country");
+		it = countries.iterator();
+		JsonPath js = new JsonPath(response.asString());
+		listsize = js.getInt("list.size()");
+	}
     
-    
-    @When("User makes a request with API key and wrong City name verify the response")
-    public void user_makes_a_request_with_API_key_and_wrong_City_name_verify_the_response() {
-    	Response response = given()
+	@Then("Verify the response has a list consisting of multiple countries")
+	public void verify_the_response_has_a_list_consisting_of_multiple_countries() {
+	    
+		while(it.hasNext())
+		{
+			System.out.println("Country under which the city is present : "+ it.next());
+		}
+	}
+	
+	@When("User passes the wrong city name {string} and country code {string} but correct app id {string} to the open weather api")
+	public void user_passes_the_wrong_city_name_and_country_code_but_correct_app_id_to_the_open_weather_api(String incorrect_city_name, String incorrect_country_code, String app_id) {
+		
+		response = given()
                 .header("Content-Type","application/json")
-                .param("q", "Londons,GB")
-            .param("appid", "1cb6ace31e50401f28b864f0b23fdc68")
+                .param("q", incorrect_city_name,incorrect_country_code)
+            .param("appid", app_id)
                .when()
                  .get("/weather");
-       	
-       	int code = response.getStatusCode();
-       	Assert.assertEquals(code, 404);
-    }
+    	error_code = response.getStatusCode();
+    	message = response.getBody().path("message");
+	}
 
-    
-    @When("User makes a request with API key and wrong Country code verify the response")
-    public void user_makes_a_request_with_API_key_and_wrong_Country_code_verify_the_response() {
-    	Response response = given()
-                .header("Content-Type","application/json")
-                .param("q", "London,GS")
-            .param("appid", "1cb6ace31e50401f28b864f0b23fdc68")
-               .when()
-                 .get("/weather");
-       	
-       	int code = response.getStatusCode();
-       	Assert.assertEquals(code, 404);
-    }
+	@Then("Verify the response code is {int} for wrong city name and country code")
+	public void verify_the_response_code_is_for_wrong_city_name_and_country_code(int status_code) {
 
-    
-    @When("User makes a request with wrong API key verify the response")
-    public void user_makes_a_request_with_wrong_API_key_verify_the_response() {
-    	Response response = given()
+       	Assert.assertEquals(status_code, error_code);
+       	Assert.assertEquals(message,"city not found");
+	}
+
+	
+	@When("User makes a request with city name {string} and incorrect app id {string} to the open weather api")
+	public void user_makes_a_request_with_city_name_and_incorrect_app_id_to_the_open_weather_api(String city_name, String incorrect_app_id) {
+		
+		response = given()
                 .header("Content-Type","application/json")
-                .param("q", "London,GB")
-            .param("appid", "1cb6ace31e50401f28b864f0b23fdc69")
+                .param("q", city_name)
+            .param("appid", incorrect_app_id)
                .when()
                  .get("/weather");
-       	
-    	String message = response.getBody().path("message");
-       	int code = response.getStatusCode();
-       	Assert.assertEquals(code, 401);
-       	Assert.assertEquals(message, "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.");
-    }
+    	error_code = response.getStatusCode();
+    	message = response.getBody().path("message");
+	}
+
+	@Then("Verify the response code is {int} for incorrect app id")
+	public void verify_the_response_code_is_for_incorrect_app_id(int status_code) {
+		
+		Assert.assertEquals(status_code, error_code);
+		Assert.assertEquals(message,"Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.");
+	}
 }
--- src/test/resources/featurefile/OpenWeatherAPITest.feature
+++ src/test/resources/featurefile/OpenWeatherAPITest.feature
@@ -3,26 +3,27 @@ Feature: Open Weather API Test Scenarios
 
 Scenario: To fetch the current weather of city as London and country code as GB
 
-		Given Current Weather URL is running
-    When User makes a request with API key and City name and Country code verify the response
+		Given Open Weather API is up and running
+    When User passes the city name "London" and country code "GB" and app id "1cb6ace31e50401f28b864f0b23fdc68" to the open weather api
+    Then Verify the response code is 200
+    And Verify the country code and country id in response body is also "GB" and 2019646 respectively
     
-Scenario: To test that the city name is not unique and present in multiple countries
+Scenario: To verify that the city is not unique and present under multiple countries
 
-		Given Current Weather URL is running
-    When User makes a request with API key and City name verify the response
+		Given Open Weather API is up and running
+    When User passes the city name "London" and app id "1cb6ace31e50401f28b864f0b23fdc68" to the open weather api
+    Then Verify the response has a list consisting of multiple countries
     
-Scenario: To verify the response when city name is not correct
+Scenario: To verify the response when city name and country code is not correct
 
-		Given Current Weather URL is running
-    When User makes a request with API key and wrong City name verify the response
-    
-Scenario: To verify the response when country code is not correct
-
-		Given Current Weather URL is running
-    When User makes a request with API key and wrong Country code verify the response
+		Given Open Weather API is up and running
+    When User passes the wrong city name "ABCD" and country code "GS" but correct app id "1cb6ace31e50401f28b864f0b23fdc68" to the open weather api
+    Then Verify the response code is 404 for wrong city name and country code
     
 Scenario: To verify the response when app id is not correct
 
-		Given Current Weather URL is running
-    When User makes a request with wrong API key verify the response
+		Given Open Weather API is up and running
+    When User makes a request with city name "London" and incorrect app id "1cb6ace31e50401f28b864f0b23fdc69" to the open weather api
+    Then Verify the response code is 401 for incorrect app id
+    
     
